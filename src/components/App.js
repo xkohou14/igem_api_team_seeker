ecdecb34e8b359806939fd48463df6d3ff3679da
Petr Kohout <xkohou14@stud.fit.vutbr.cz>
2020-04-10 23:09:14 +0200

    Fields of search
    Signed-off-by: Petr Kohout <xkohou14@stud.fit.vutbr.cz>

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,23 @@ import SearchBar from "./SearchBar";
 
 class App extends Component {
     constructor(props) {
-        super(props)
+        super(props);
         this.state = {
             isLoaded: false,
-            items: []
+            items: [],
+            biobricks_structure : [],
+            teams_structure : []
         }
     }
 
     componentDidMount() {
-        this.setState({isLoaded: true})
+        this.setState({isLoaded: true});
 
         fetch("http://localhost:3001/teams")
             .then(response => response.json())
             .then(responseData => {
                 this.setState({
+                    isLoaded : true,
                     items: responseData.map(item => ({
                         title: item.title,
                         wiki: item.wiki,
@@ -25,21 +28,36 @@ class App extends Component {
                         description: item.abstract,
                     }))
                 })
+            });
 
-                // console.log(this.state.items[9].title)
+        fetch("http://localhost:3001/teams/structure")
+            .then(response => response.json())
+            .then(responseData => {
+                console.log("Structure : " + responseData);
+                this.setState({
+                    ...this.state,
+                    teams_structure : responseData
+                })
+            });
 
-                // for (let i = 0; i < 11; i++) {
-                //     if (typeof this.state.items[i].title != 'undefined') {
-                //         console.log(this.state.items[i].title.toLowerCase().includes("cell"))
-                //     }
-                // }
+        fetch("http://localhost:3001/biobricks/structure")
+            .then(response => response.json())
+            .then(responseData => {
+                console.log("Structure : " + responseData);
+                this.setState({
+                    ...this.state,
+                    biobricks_structure : responseData
+                })
+            });
+    }
 
-            })
+    clickMaster() {
+        console.log("Someone clicked me.");
     }
 
     render() {
         if(this.state.isLoaded) {
-            // console.log(this.state.items)
+            console.log(this.state.items)
         }
 
         return (
@@ -47,7 +65,7 @@ class App extends Component {
             <div className="App">
                 <nav>
                     <h1 className="App-header">Team Seeker</h1>
-                    <SearchBar items={this.state.items}/>
+                    <SearchBar items={this.state.items} master={this} team_structure={this.state.teams_structure} biobricks_structure={[]}/>
                     {/*<p>{text}</p>*/}
                 </nav>
             </div>
