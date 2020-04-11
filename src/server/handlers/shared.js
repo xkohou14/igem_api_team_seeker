484966660b298698f2f5b833a0bc919cb469c71b
Petr Kohout <xkohou14@stud.fit.vutbr.cz>
2020-04-11 18:42:49 +0200

    Authenticator, Shared variables, package.json updated
    Signed-off-by: Petr Kohout <xkohou14@stud.fit.vutbr.cz>

--- src/server/handlers/shared.js
+++ src/server/handlers/shared.js
@@ -3,23 +3,11 @@
  */
 
 const elasticSearch = require('elasticsearch');
-const nodemailer = require('nodemailer');
-const randomstring = require('randomstring');
 
 module.exports = {
     JWT_KEY : 'myPrivateKeyiGemSeeker',
     client : new elasticSearch.Client({
         host : 'https://elasticsearch.kusik.net',
         log: 'trace'
-    }),
-    mail_transporter : nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: 'info.wap.fit@gmail.com',
-            pass: 'wap-sender'
-        }
-    }),
-    wap_mail: 'info.wap.fit@gmail.com',
-    url: "localhost:3001/",
-    randomstring : randomstring
+    })
 }
