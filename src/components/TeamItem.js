77786b14415d36f5d3bcf1d91d90c6750dc5b98c
Gabriela <xchmel27@stud.fit.vutbr.cz>
2020-04-11 12:58:35 +0200

    style update, search fixed, hidden results without title

--- src/components/TeamItem.js
+++ src/components/TeamItem.js
@@ -1,13 +1,15 @@
 import React from "react";
+import './TeamItem.css'
 
 // Component for displaying one team card in the main page
 function TeamItem(props) {
     return (
         <div className="team-item">
-            <h1>{props.item.title}</h1>
-            <p>{props.item.description}</p>
-            <p>year: {props.item.year}</p>
-            <p>wiki: {props.item.wiki}</p>
+            <h2 className="team-item-header">
+                <span className="span">{props.item.year + " "}</span>
+                {" " + props.item.title}</h2>
+            <p className="team-item-descr">{props.item.description}</p>
+            <p className="team-item-wiki"> wiki: {props.item.wiki}</p>
             <hr/>
         </div>
     )
