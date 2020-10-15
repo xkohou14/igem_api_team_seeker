3ad1a64b0ccfd3a9f1c0e533ddf3bd4f3b866c2e
Petr Kohout <xkohou14@stud.fit.vutbr.cz>
2020-10-15 20:38:57 +0200

    Updates

---
+++ precache-manifest.e4cdb9f7a81b4ee57671b0d5a90d524b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "952f0bc6602fd15e1113f80264425bd9",
+    "url": "/my-igem_api_team_seeker/index.html"
+  },
+  {
+    "revision": "98b9fac6d36880f8a62e",
+    "url": "/my-igem_api_team_seeker/static/css/main.581217f2.chunk.css"
+  },
+  {
+    "revision": "f57bbeb52138cc3aab39",
+    "url": "/my-igem_api_team_seeker/static/js/2.7aa98b47.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-igem_api_team_seeker/static/js/2.7aa98b47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98b9fac6d36880f8a62e",
+    "url": "/my-igem_api_team_seeker/static/js/main.419b2254.chunk.js"
+  },
+  {
+    "revision": "c0039232a217c04787ab",
+    "url": "/my-igem_api_team_seeker/static/js/runtime-main.5006e18a.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/my-igem_api_team_seeker/precache-manifest.e4cdb9f7a81b4ee57671b0d5a90d524b.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-igem_api_team_seeker/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
